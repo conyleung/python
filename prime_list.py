4abbabd3c73dc8c279869e91f4bbb2d191754fd6
cony <conylueng@github.com>
2019-07-12 17:10:33 +0800

    提交

--- prime_list.py
+++ prime_list.py
@@ -2,10 +2,10 @@
 #-*- coding: utf-8 -*-
 import math
 a = int(input('please input a number:'))
-pr = list(range(0,a+1))
+pr = [x for x in range(0,a+1)]
 impr = []
 if a < 2:
-    print(pr)
+    print([])
 for i in pr:
     for k in range(2,int(math.sqrt(i))+1):
         if i % k == 0:
@@ -13,5 +13,5 @@ for i in pr:
             break
 for i in impr:
     pr.remove(i)
-print(pr)
+    print(pr)
 
